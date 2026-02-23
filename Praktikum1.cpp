02d2d592d2199204493e0a40a32c97def7cdb4ca
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-02-23 16:01:52 +0700

    age =19;

--- Praktikum1.cpp
+++ Praktikum1.cpp
@@ -8,9 +8,9 @@ string nama;
 int main(){ //program utama
     int age; //variable local
     int age1; //variable local
-    age = 20;
+    age = 19;
     age1 = 15;
-
+    
     cout << "Halo world" << endl;
     cout << 99 << endl;
     cout << age;
